1247db56fec622f973d374e582ff645300ab9fee
João Pedro MW <80231608+Joao-Pedro-MW@users.noreply.github.com>
2022-09-09 00:01:35 -0300

    Create Espelho.java

---
+++ PRJT1/atividade_avaliativa_19_08/Espelho.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade_avaliativa_19_08;
+
+/**
+ *
+ * @author Joao Pedro MW
+ */
+public class Espelho  {
+    String tipo_vidro;
+    int espelho_altura,espelho_largura;
+
+    public String getTipo_vidro() {
+        return tipo_vidro;
+    }
+
+    public void setTipo_vidro(String tipo_vidro) {
+        this.tipo_vidro = tipo_vidro;
+    }
+
+    public float getEspelho_altura() {
+        return espelho_altura;
+    }
+
+    public void setEspelho_altura(int espelho_altura) {
+        this.espelho_altura = espelho_altura;
+    }
+
+    public float getEspelho_largura() {
+        return espelho_largura;
+    }
+
+    public void setEspelho_largura(int espelho_largura) {
+        this.espelho_largura = espelho_largura;
+    }
+
+    
+    
+}
